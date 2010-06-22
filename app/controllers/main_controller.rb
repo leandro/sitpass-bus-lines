2482f6adb3ea0ecaca970e4993041da1bcf30f4f
Leandro Camargo <leandroico@gmail.com>
2010-06-22 01:00:01 -0300

    the first working version: 0.1

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,10 +4,17 @@ class MainController < ApplicationController
 
   def index
 
-    if request.post?
+    if @show_list = !request.get?
       @linhas = BusLine.all
     else
-      @linhas = BusLine.all
+      words = params[:q].downcase.slugfy_words.split(/\s+/)
+      wild_words = words.map {|e| "%#{e}%"}
+      query = words.map {|e| "?"}.join(' OR name_slug LIKE ')
+      @linhas = BusLine.all(:conditions => ["name_slug LIKE #{query}"] + wild_words)
+      query = words.map {|e| "?"}.join(' OR logradouro_slug LIKE ')
+      query = " #{query} OR bairro_slug LIKE " + words.map {|e| "?"}.join(' OR LIKE bairro_slug ')
+      @linhas.push(*BusLinePlace.all(:conditions => ["logradouro_slug LIKE #{query}"] + (wild_words * 2)).map(&:bus_line))
+      @linhas = @linhas.uniq.sort {|a,b| a.code <=> b.code}
     end
 
   end
