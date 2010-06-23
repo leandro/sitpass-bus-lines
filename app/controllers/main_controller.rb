bd06af6bda6f27e5303cec1c791050265746e0bc
Leandro Camargo <leandroico@gmail.com>
2010-06-23 01:59:06 -0300

    improving multi-words search

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,16 +7,40 @@ class MainController < ApplicationController
     terms = params[:q].to_s
 
     if @show_list = terms.empty?
-      @linhas = BusLine.all
+      @lines = BusLine.all
     else
       words = terms.downcase.slugfy_words.split(/\s+/)
-      wild_words = words.map {|e| "%#{e}%"}
-      query = words.map {|e| "?"}.join(' OR name_slug LIKE ')
-      @linhas = BusLine.all(:conditions => ["name_slug LIKE #{query}"] + wild_words)
+      wildcard_words = words.map {|e| "%#{e}%"}
+      query = words.map {|e| "?"}.join(' AND name_slug LIKE ')
+      @lines = BusLine.all(:conditions => ["name_slug LIKE #{query}"] + wildcard_words)
       query = words.map {|e| "?"}.join(' OR logradouro_slug LIKE ')
-      query = " #{query} OR bairro_slug LIKE " + words.map {|e| "?"}.join(' OR LIKE bairro_slug ')
-      @linhas.push(*BusLinePlace.all(:conditions => ["logradouro_slug LIKE #{query}"] + (wild_words * 2)).map(&:bus_line))
-      @linhas = @linhas.uniq.sort {|a,b| a.code <=> b.code}
+      query = " #{query} OR bairro_slug LIKE " + words.map {|e| "?"}.join(' OR bairro_slug LIKE ')
+      places = BusLinePlace.all(:conditions => ["logradouro_slug LIKE #{query}"] + (wildcard_words * 2), :order => 'bus_line_id')
+      grouped_places = {}
+
+      while places.any?
+        place = places.shift
+        (grouped_places[place.bus_line_id] ||= []) << place
+      end
+
+      @matched_lines = []
+      if words.size > 1
+
+        for bus_line_id in grouped_places.keys
+          all_places_in_one = grouped_places[bus_line_id].map {|e| [e.logradouro_slug, e.bairro_slug]}.flatten.join(' ')
+          matched = 0
+          for word in words
+            matched += 1 if all_places_in_one =~ /#{word}/
+          end
+          @matched_lines << grouped_places[bus_line_id].first.bus_line if matched == words.size
+        end
+
+      else
+        @matched_lines = BusLine.find(grouped_places.keys)
+      end
+
+      @lines.push(*@matched_lines)
+      @lines = @lines.uniq.sort {|a,b| a.code <=> b.code}
     end
 
   end
